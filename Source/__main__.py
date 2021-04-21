1ad118ec4f7db513d126388ffc982876c53081b4
Matteo <matteo@capasso.dev>
2021-04-21 10:55:19 -0400

    Refactor and updated to do list

--- Source/__main__.py
+++ Source/__main__.py
@@ -11,9 +11,10 @@ KAGGLE_DATASETS_URL_LIST = [ #Please ensure your URL list matches the same forma
         'https://www.kaggle.com/rashikrahmanpritom/heart-attack-analysis-prediction-dataset',
         'https://www.kaggle.com/ajaypalsinghlo/world-happiness-report-2021',
         'https://www.kaggle.com/iabhishekofficial/mobile-price-classification',
-        'https://www.kaggle.com/gpreda/reddit-vaccine-myths'
+        'https://www.kaggle.com/gpreda/reddit-vaccine-myths',
+        'https://www.kaggle.com/rsrishav/youtube-trending-video-dataset'
         ]
-KAGGLE_DATASETS_LOCATION = r'Archive' #Default is relative file path to working directory.
+KAGGLE_DATASETS_LOCATION = r'Archive' #Relative file path to working directory.
 KAGGLE_REMOVE_JSON = False
 #############################(Configurations - End)#############################
 
@@ -36,27 +37,13 @@ def extractURLData(url:str) -> tuple: #Pulls author and dataset name from kaggle
             break
     return datasetNameString, datasetAuthorString
         
-#! NOTE: Class use ommitted at time of creation b/c Kaggle metadata API is not fixed yet.
-class kaggleMetadadata:
-    #Classes Example: https://sphinxcontrib-napoleon.readthedocs.io/en/latest/example_google.html
-    #Classes Docs: https://docs.python.org/3/tutorial/classes.html
-    #Kaggle Metadata Ex: https://www.kaggle.com/rashikrahmanpritom/heart-attack-analysis-prediction-dataset/metadata
-
-    def __init__(self,author,license,updateFrequency,dateCreated,lastOnlineUpdate,lastOfflineUpdate,currentOnlineVersion,currentOfflineVersion): 
-        #__init__ is a constructor for new object creation specific to class
-        #self is used to show variables belong to this class
-        #instaces are like states for a group (e.g. water is LIQUId, water is SOLID)
-        self.author = author
-        self.license = license
-        self.updateFrequency = updateFrequency
-        self.dateCreated = dateCreated
-        self.lastOnlineUpdate = lastOnlineUpdate
-        self.lastOfflineUpdate = lastOfflineUpdate
-        self.currentOnlineVersion = currentOnlineVersion
-        self.currentOfflineVersion = currentOfflineVersion
+##ToDo:
+# kaggle authenticaion validation & common folder
+# check to see if download interrupted, check for it based off .zip
+# warning if last update was some time ago
    
 def main():
-    #Converting config URL list to convenient datastructure.
+    #Converting config's URL list to convenient datastructure.
     trackedDatasets = []
     for i in KAGGLE_DATASETS_URL_LIST:
         trackedDatasets.append(extractURLData(i))
@@ -65,6 +52,7 @@ def main():
     #Note: Date naming convention is YYYY-MM-DD
     for datasetName, datasetAuthor in trackedDatasets:        
         individualDatasetLocation = os.path.join(KAGGLE_DATASETS_LOCATION,datasetName)
+        #! Mark process start here
         print('Individual dataset location: ' + individualDatasetLocation)#temp
 
         try: #Checking online version's latest date.
@@ -88,7 +76,7 @@ def main():
             kaggleSourceName = datasetAuthor + '/' + datasetName
             os.system(kaggleDownloadCmd(kaggleSourceName, newDateFolderPath, unzip=True))
             #Note: kaggle unzipping uses parrallelism/concurrency natively. 
-
+        #! Mark process end here
         time.sleep(5) #Prevent API spam
 
 if __name__ == '__main__':
--- Source/utility.py
+++ Source/utility.py
@@ -15,6 +15,23 @@ def removeByFileExtension(extension:str, directory:str):
         if file[-len(extension):] == extension:
             os.unlink(os.path.join(directory, file)) 
 
+# class kaggleMetadadata:
+#     #Classes Example: https://sphinxcontrib-napoleon.readthedocs.io/en/latest/example_google.html
+#     #Classes Docs: https://docs.python.org/3/tutorial/classes.html
+#     #Kaggle Metadata Ex: https://www.kaggle.com/rashikrahmanpritom/heart-attack-analysis-prediction-dataset/metadata
+
+#     def __init__(self,author,license,updateFrequency,dateCreated,lastOnlineUpdate,lastOfflineUpdate,currentOnlineVersion,currentOfflineVersion): 
+#         #__init__ is a constructor for new object creation specific to class
+#         #self is used to show variables belong to this class
+#         #instaces are like states for a group (e.g. water is LIQUId, water is SOLID)
+#         self.author = author
+#         self.license = license
+#         self.updateFrequency = updateFrequency
+#         self.dateCreated = dateCreated
+#         self.lastOnlineUpdate = lastOnlineUpdate
+#         self.lastOfflineUpdate = lastOfflineUpdate
+#         self.currentOnlineVersion = currentOnlineVersion
+#         self.currentOfflineVersion = currentOfflineVersion
+
 if __name__ == '__main__':
     pass
-    

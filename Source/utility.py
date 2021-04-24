f5b2b8456c2e4178a248b816a20fa88d58c4d97f
Matteo <matteo@capasso.dev>
2021-04-24 15:09:25 -0400

    Added elapsed time calculator for use in logs

--- Source/utility.py
+++ Source/utility.py
@@ -16,6 +16,20 @@ def removeByFileExtension(extension:str, directory:str):
         if file[-len(extension):] == extension:
             os.unlink(os.path.join(directory, file)) 
 
+def elapsedTimeCalculator(startTime:float, endTime:float, decRound:int=2) -> str:
+    elapsedTimeSecs = endTime - startTime
+    elapsedTimeMins = elapsedTimeSecs/60
+    elapsedTimeHours = elapsedTimeMins/60
+    elapsedTimeDays = elapsedTimeHours/24
+    if elapsedTimeDays >= 1: #Time as unit of days. 
+        return str(round(elapsedTimeDays, decRound)) + ' days'
+    elif elapsedTimeHours >= 1: #Time as unit of hours.
+        return str(round(elapsedTimeHours, decRound)) + ' hours'
+    elif elapsedTimeMins >= 1: #Time as unit of minutes.
+        return str(round(elapsedTimeMins,decRound)) + ' minutes'
+    else: #Time as unit of seconds.
+        return str(round(elapsedTimeSecs,decRound)) + ' seconds'
+
 # class kaggleMetadadata:
 #     #Classes Example: https://sphinxcontrib-napoleon.readthedocs.io/en/latest/example_google.html
 #     #Classes Docs: https://docs.python.org/3/tutorial/classes.html
@@ -35,4 +49,4 @@ def removeByFileExtension(extension:str, directory:str):
 #         self.currentOfflineVersion = currentOfflineVersion
 
 if __name__ == '__main__':
-    pass
+    print(elapsedTimeCalculator(0, 9))

cce91df732e144c6dd6c9ab7e1cc0b045091c7a2
Matteo <matteo@capasso.dev>
2021-04-26 15:22:27 -0400

    Added better demo

--- Source/__main__.py
+++ Source/__main__.py
@@ -4,7 +4,6 @@ from kaggle_API import kaggleDownloadCmd, kaggleRecentVersionDate
 from utility import elapsedTimeCalculator, extractURLData, folderSizeAmount, byteUnitConverter
         
 ##ToDo:
-# create folder for logs based on date
 # add datasets location pre-check for configs
 # documentation polish up
 # check if works on linux
@@ -12,7 +11,6 @@ from utility import elapsedTimeCalculator, extractURLData, folderSizeAmount, byt
 # put file_preprocessing to YT project / other
 # verify alternative dataset locations works
 # verify sleeping does not hit last download
-# add day to start and end
 # add usage statistics
    
 def main():
@@ -20,44 +18,44 @@ def main():
 
     ############################(Configurations - Start)############################
     KAGGLE_DATASETS_URL_LIST = [ #URL Format (required): https://www.kaggle.com/<author>/<datasetname>
-            #'https://www.kaggle.com/rsrishav/youtube-trending-video-dataset', #CC0 1.0 Universal (CC0 1.0)
-            #'https://www.kaggle.com/hgultekin/covid19-stream-data', #Database Contents License (DbCL) v1.0
-            #'https://www.kaggle.com/cityofLA/los-angeles-parking-citations', #Open Data Commons Open Database License (ODbL) v1.0
-            #'https://www.kaggle.com/chaibapat/slogan-dataset', #Database Contents License (DbCL) v1.0
-            #'https://www.kaggle.com/camnugent/sandp500', #CC0 1.0 Universal (CC0 1.0)
+            'https://www.kaggle.com/rsrishav/youtube-trending-video-dataset', #CC0 1.0 Universal (CC0 1.0)
+            'https://www.kaggle.com/hgultekin/covid19-stream-data', #Database Contents License (DbCL) v1.0
+            'https://www.kaggle.com/cityofLA/los-angeles-parking-citations', #Open Data Commons Open Database License (ODbL) v1.0
+            'https://www.kaggle.com/chaibapat/slogan-dataset', #Database Contents License (DbCL) v1.0
+            'https://www.kaggle.com/camnugent/sandp500', #CC0 1.0 Universal (CC0 1.0)
             'https://www.kaggle.com/dhruvildave/github-commit-messages-dataset' #Open Data Commons Attribution License (ODC-By) v1.0
             'https://www.kaggle.com/gauravduttakiit/covid-19',
-            # 'https://www.kaggle.com/paultimothymooney/coronavirus-in-italy',
-            # 'https://www.kaggle.com/gpreda/all-covid19-vaccines-tweets',
-            # 'https://www.kaggle.com/gpreda/pfizer-vaccine-on-reddit',
-            # 'https://www.kaggle.com/shivamb/netflix-shows',
-            # 'https://www.kaggle.com/gpreda/reddit-wallstreetsbets-posts',
-            # 'https://www.kaggle.com/shivkumarganesh/politifact-factcheck-data',
-            # 'https://www.kaggle.com/aaron7sun/stocknews',
-            # 'https://www.kaggle.com/jealousleopard/goodreadsbooks',
-            # 'https://www.kaggle.com/dhruvildave/wikibooks-dataset',
-            # 'https://www.kaggle.com/imsparsh/musicnet-dataset',
-            # 'https://www.kaggle.com/datasnaek/chess',
-            # 'https://www.kaggle.com/shivamb/netflix-shows',
-            # 'https://www.kaggle.com/unsdsn/world-happiness',
-            # 'https://www.kaggle.com/arashnic/hr-analytics-job-change-of-data-scientists',
-            # 'https://www.kaggle.com/tencars/392-crypto-currency-pairs-at-minute-resolution',
-            # 'https://www.kaggle.com/antgoldbloom/covid19-data-from-john-hopkins-university',
-            # 'https://www.kaggle.com/new-york-state/nys-currently-licensed-real-estate-appraisers',
-            # 'https://www.kaggle.com/new-york-state/nys-city-of-albany-building-permits-issued',
-            # 'https://www.kaggle.com/sobhanmoosavi/us-accidents',
-            # 'https://www.kaggle.com/gpreda/covid-world-vaccination-progress',
-            # 'https://www.kaggle.com/arthurio/italian-vaccination',
-            # 'https://www.kaggle.com/kaggle/meta-kaggle',
-            # 'https://www.kaggle.com/dhruvildave/github-commit-messages-dataset',
-            # 'https://www.kaggle.com/yamaerenay/spotify-dataset-19212020-160k-tracks',
-            # 'https://www.kaggle.com/fireballbyedimyrnmom/us-counties-covid-19-dataset',
-            # 'https://www.kaggle.com/dhruvildave/billboard-the-hot-100-songs',
-            # 'https://www.kaggle.com/komalkhetlani/population-growth-annual',
-            # 'https://www.kaggle.com/austinreese/craigslist-carstrucks-data',
-            # 'https://www.kaggle.com/paultimothymooney/stock-market-data',
-            # 'https://www.kaggle.com/brendan45774/hollywood-most-profitable-stories',
-            # 'https://www.kaggle.com/dhruvildave/top-play-store-games',
+            'https://www.kaggle.com/paultimothymooney/coronavirus-in-italy',
+            'https://www.kaggle.com/gpreda/all-covid19-vaccines-tweets',
+            'https://www.kaggle.com/gpreda/pfizer-vaccine-on-reddit',
+            'https://www.kaggle.com/shivamb/netflix-shows',
+            'https://www.kaggle.com/gpreda/reddit-wallstreetsbets-posts',
+            'https://www.kaggle.com/shivkumarganesh/politifact-factcheck-data',
+            'https://www.kaggle.com/aaron7sun/stocknews',
+            'https://www.kaggle.com/jealousleopard/goodreadsbooks',
+            'https://www.kaggle.com/dhruvildave/wikibooks-dataset',
+            'https://www.kaggle.com/imsparsh/musicnet-dataset',
+            'https://www.kaggle.com/datasnaek/chess',
+            'https://www.kaggle.com/shivamb/netflix-shows',
+            'https://www.kaggle.com/unsdsn/world-happiness',
+            'https://www.kaggle.com/arashnic/hr-analytics-job-change-of-data-scientists',
+            'https://www.kaggle.com/tencars/392-crypto-currency-pairs-at-minute-resolution',
+            'https://www.kaggle.com/antgoldbloom/covid19-data-from-john-hopkins-university',
+            'https://www.kaggle.com/new-york-state/nys-currently-licensed-real-estate-appraisers',
+            'https://www.kaggle.com/new-york-state/nys-city-of-albany-building-permits-issued',
+            'https://www.kaggle.com/sobhanmoosavi/us-accidents',
+            'https://www.kaggle.com/gpreda/covid-world-vaccination-progress',
+            'https://www.kaggle.com/arthurio/italian-vaccination',
+            'https://www.kaggle.com/kaggle/meta-kaggle',
+            'https://www.kaggle.com/dhruvildave/github-commit-messages-dataset',
+            'https://www.kaggle.com/yamaerenay/spotify-dataset-19212020-160k-tracks',
+            'https://www.kaggle.com/fireballbyedimyrnmom/us-counties-covid-19-dataset',
+            'https://www.kaggle.com/dhruvildave/billboard-the-hot-100-songs',
+            'https://www.kaggle.com/komalkhetlani/population-growth-annual',
+            'https://www.kaggle.com/austinreese/craigslist-carstrucks-data',
+            'https://www.kaggle.com/paultimothymooney/stock-market-data',
+            'https://www.kaggle.com/brendan45774/hollywood-most-profitable-stories',
+            'https://www.kaggle.com/dhruvildave/top-play-store-games',
             ]
     KAGGLE_DATASETS_LOCATION = r'Archive'
         #Relative or absolute file paths accepted.
@@ -77,7 +75,8 @@ def main():
     logger.setLevel(logging.DEBUG)
     
     #File Specific Logging Configurations
-    fh = logging.FileHandler(os.path.join('Logs',f"{time.strftime('%Y-%m-%d')}.log"))
+    currentDate = f"{time.strftime('%Y-%m-%d')}"
+    fh = logging.FileHandler(os.path.join('Logs',f"{currentDate}.log"))
     fh.setLevel(logging.DEBUG)
     fh.setFormatter(formatter)
     logger.addHandler(fh)
@@ -97,7 +96,7 @@ def main():
         KAGGLE_DATASETS_LOCATION = os.path.abspath(KAGGLE_DATASETS_LOCATION)
 
     startSize = folderSizeAmount(os.path.abspath(KAGGLE_DATASETS_LOCATION))
-    logger.info('##############(Program Start)###########')
+    logger.info(f"#######################(Program Start (Date: {currentDate}))#######################")
 
     #Converting config's URL list to convenient datastructure.
     try:
@@ -111,9 +110,8 @@ def main():
 
 
     ######################(Version Control & Updating - Start)######################  
-    failedOnlineRetrivalCounter = 1
-    failedOnlineRetrivalAttempts = 3
-    timerPosition = 0
+    failedOnlineRetrivalAttempts = 2 #Acts as three attempts.
+    timerPosition = failedOnlineRetrivalCounter = 0
     for datasetName, datasetAuthor in trackedDatasets:        
         logger.debug(f"Initiating process: {datasetName}/{datasetAuthor}")
 
@@ -134,8 +132,7 @@ def main():
             else: 
                 logger.error(f"[Line: {sys.exc_info()[-1].tb_lineno}] [Problem: {sys.exc_info()[1]}]")
                 logger.critical(f"Max online retrival attempts reached, exiting.")
-                time.sleep(5)
-                sys.exit()
+                break
         
         try: #Checking offline version's latest date.
             kaggleOfflineVersion = -1
@@ -153,10 +150,10 @@ def main():
             if kaggleOnlineVersion != kaggleOfflineVersion:
                 logger.info(f"Outdated version: {datasetName}/{datasetAuthor}")
                 newDateFolderPath = os.path.join(individualDatasetPath, kaggleOnlineVersion) 
-                os.makedirs(newDateFolderPath)
+                os.makedirs(newDateFolderPath) #! temp, uncomment
                 kaggleSourceName = datasetAuthor + '/' + datasetName
                 logger.info(f"Starting download: {datasetName}/{datasetAuthor}")
-                print(kaggleDownloadCmd(kaggleSourceName, newDateFolderPath,
+                os.system(kaggleDownloadCmd(kaggleSourceName, newDateFolderPath, #! temp, change print out
                 quiet=not(CONSOLE_TEXT_OUTPUT), unzip=UNZIP_DATASETS)) 
                 logger.info(f"Finished download: {datasetName}/{datasetAuthor}")
             else:
@@ -168,8 +165,6 @@ def main():
         
         #Sleeping to prevent API spam.
         timerPosition += 1
-        logger.debug('timerPosition: ' + str(timerPosition))
-        logger.debug('len(trackedDatasets: ' + str(len(trackedDatasets)))
         if timerPosition < len(trackedDatasets):
             logger.debug(f"Sleeping: {SLEEP_TIME} seconds.")
             time.sleep(SLEEP_TIME)
@@ -180,7 +175,7 @@ def main():
     endTime = time.time()
     endSize = folderSizeAmount(os.path.abspath(KAGGLE_DATASETS_LOCATION))
     sizeDifference = endSize - startSize #These units are in bytes.
-    logger.info('##############(Program End)#############')
+    logger.info(f"########################(Program End (Date: {currentDate}))########################")
     logger.info(f"Elapsed time: {elapsedTimeCalculator(startTime,endTime)}")
     logger.info(f"Added data: {byteUnitConverter(sizeDifference)}")
     #############################(Postprocessing - End)#############################

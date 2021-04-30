3ad9950c4ff372734df024bee7d4c3da84b5c345
Matteo <matteo@capasso.dev>
2021-04-30 13:21:04 -0400

    Revised and fixed quickstart prechecking

--- quickstart.py
+++ quickstart.py
@@ -1,14 +1,26 @@
-import os, sys, time
-from Source\precheck.py import *
+#!/user/bin/python3.9.1
 
+#     License Type: GNU General Public License v3.0
+#     'Kaggle Autoupdater', automated file version control for Kaggle.
+#     Copyright (C) 2021 Matteo Capasso (matteo@capasso.dev)
+
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import os, sys, time
+sys.path.append("Source")
+from Source.precheck import *
+
+#Minimum Python3 version: 3.9.1
 pythonMajorVersion = a = 3
 pythonMinorVersion = b = 9
 pythonMicroVersion = c = 1
 pythonVersion = f"{a}.{b}.{c}"
 
-os.system('cls')
-try:
+try: #This process serves as a pre-check to ensure user has proper setup.
+    os.system('cls')
     if approxProgramRuns() > 2: 
+        from Source.__main__ import main
         main()
     else:
         #Display Information
@@ -17,22 +29,23 @@ try:
         + ' after a few program runs.*' + "\n")
         os.system('pause')
         os.system('cls')
-        print(requirementsDisplay())
+        print(requirementsDisplay(pythonVersion))
         os.system('pause')
         os.system('cls')
 
         #Precheck Process
-        if pythonVersionValidate(pythonMajorVersion,
-        pythonMinorVersion,pythonMicroVersion) == False:
+        if pythonVersionValidate(a,b,c) == False:
             raise AssertionError('Invalid Python3 version.'
             + f" Requires v{pythonVersion} or greater.")
-        try:
-            if kaggleModuleAndTokenExistence == True:
-                main()
-        except:
-            print('oof')
-            
+        if kaggleModuleAndTokenExistence() == False:
+            import kaggle #Intentionally breaking.
+
+        #Passed Precheck Process
+        from Source.__main__ import main
+        main()
+
 except: 
-    print(f"Problem: {sys.exc_info()[1]}")
+    print(f"Problem Type: {sys.exc_info()[0]}")
+    print(f"Problem Value: {sys.exc_info()[1]}")
     os.system('pause')
     sys.exit()
